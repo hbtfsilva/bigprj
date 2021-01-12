c4e6c635f2541184e0f740cf5a68d9a8b3b47c51
Herbert F. Silva <42248470+hbtfsilva@users.noreply.github.com>
2021-01-12 13:26:59 -0300

    Exercícios "js"

---
+++ funcaoemquasetudo.js
@@ -0,0 +1,6 @@
+console.log(typeof Object)
+
+class   Produto {} // qualuqer classe que eu criar em JS será uma função.
+console.log(typeof Produto)
+
+// JS é altamente centrado em funções.
---
+++ organizacao.js
@@ -0,0 +1,10 @@
+console.log("Senteça de código")
+
+/* o bloco agrupar sentenças de códigos para fazer algo*/
+
+{
+    {
+        console.log("Olá");
+        console.log('Mundo!') // Padrão do curso
+    }
+}
