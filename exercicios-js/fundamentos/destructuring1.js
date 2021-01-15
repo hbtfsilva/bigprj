e8987fdcfdbabd8f818606b65c0dbaa9f4882bab
Herbert F. Silva <hbtfsilva@protonmail.com>
2021-01-15 13:53:22 -0300

    Aulas

---
+++ exercicios-js/fundamentos/destructuring1.js
@@ -0,0 +1,24 @@
+// Novo recurso do ES2015
+
+const pessoa = {
+    nome: 'Ana',
+    idade: 5,
+    endereco: {
+        logradouro: 'Rua ABC',
+        numero: 1000
+    }
+}
+
+// As chaves representa o desistruturador
+const {nome, idade} = pessoa
+console.log(nome, idade)
+
+const {nome: n, idade: i} = pessoa 
+console.log(n, 1)
+
+const { sobrenome, bemHumorado = true} = pessoa
+console.log(sobrenome, bemHumorado)
+
+const { endereco: { logradouro, numero, cep} } = pessoa
+console.log(logradouro, numero, cep)
+
---
+++ exercicios-js/fundamentos/destructuring2.js
@@ -0,0 +1,4 @@
+const [a] = [10]
+console.log(a)
+
+const [n1, , n3]
